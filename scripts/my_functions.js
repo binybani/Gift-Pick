1aefefd92e422591b48cb969ca698f69c189a382
Yubin Kim <binybani@gmail.com>
2021-04-05 16:43:47 -0700

    beanie modified contact.html, drink.html, end.html, index.html, login.html, main.html, payment.html, profile.html, my_funtion.js and starbucks.html and, add morephotos

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -16,3 +16,72 @@ function sayHello(){
     })
 }
 sayHello();
+
+function writeContact() {
+    var contactsRef = db.collection("contacts");
+    contactsRef.add({
+        code: "FRD",
+        frstname: "Yubin",
+		lastname: "Kim",             
+        cellNum: "123-456-789",
+        email: "yubin@comp1800.com",
+        picture: "peson1.jpg"
+    });
+    contactsRef.add({
+        code: "FRD",
+        frstname: "Jenny",
+		lastname: "Yuon",             
+        cellNum: "123-456-789",
+        email: "jenny@comp1800.com",
+        picture: "peson2.jpg"
+    });
+    contactsRef.add({
+        code: "COWK",
+        frstname: "Dan",
+		lastname: "Choi",             
+        cellNum: "123-456-789",
+        email: "dan@comp1800.com",
+        picture: "peson3.jpg"
+    });
+    contactsRef.add({
+        code: "COWK",
+        frstname: "Jhon",
+		lastname: "Park",             
+        cellNum: "123-456-789",
+        email: "jhon@comp1800.com",
+        picture: "peson4.jpg"
+    });
+    contactsRef.add({
+        code: "FAM",
+        frstname: "Younhee",
+		lastname: "Kim",             
+        cellNum: "123-456-789",
+        email: "younhee@comp1800.com",
+        picture: "peson5.jpg"
+    });
+    contactsRef.add({
+        code: "FAM",
+        frstname: "Changzhong",
+		lastname: "Kim",             
+        cellNum: "123-456-789",
+        email: "changzhong@comp1800.com",
+        picture: "peson6.jpg"
+    });
+    contactsRef.add({
+        code: "FAM",
+        frstname: "Yongjin",
+		lastname: "Kim",             
+        cellNum: "123-456-789",
+        email: "yongjin@comp1800.com",
+        picture: "peson7.jpg"
+    });
+    contactsRef.add({
+        code: "FAM",
+        frstname: "Junga",
+		lastname: "Yu",             
+        cellNum: "123-456-789",
+        email: "junga@comp1800.com",
+        picture: "peson8.jpg"
+    });
+}
+//writeContacts();
